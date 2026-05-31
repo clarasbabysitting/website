5e7ee12e4ad87f397234d73aa4d34820aa752be2
clarasbabysitting <claraetownsend@gmail.com>
2026-05-30 20:09:14 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -7,15 +7,13 @@
 // ============================================================
 
 const firebaseConfig = {
-  const firebaseConfig = {
-    apiKey: "AIzaSyDPqpCLfcXLOVQgRfknrB7sB3ogtN_DA1w",
-    authDomain: "babysitting-d5791.firebaseapp.com",
-    projectId: "babysitting-d5791",
-    storageBucket: "babysitting-d5791.firebasestorage.app",
-    messagingSenderId: "664714812625",
-    appId: "1:664714812625:web:f431276528d23b09fbb9f0",
-    measurementId: "G-R4N37SNN6W"
-  };
+  apiKey: "AIzaSyDPqpCLfcXLOVQgRfknrB7sB3ogtN_DA1w",
+  authDomain: "babysitting-d5791.firebaseapp.com",
+  projectId: "babysitting-d5791",
+  storageBucket: "babysitting-d5791.firebasestorage.app",
+  messagingSenderId: "664714812625",
+  appId: "1:664714812625:web:f431276528d23b09fbb9f0",
+  measurementId: "G-R4N37SNN6W"
 };
 
 // ---- Initialize Firebase ----
